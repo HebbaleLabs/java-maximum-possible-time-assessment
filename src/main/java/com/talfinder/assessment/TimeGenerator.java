474832b2006ceda706d9c431cf173a18277bb0d6
Harsha Bhargav <hbhargav@users.noreply.github.com>
2019-02-27 18:08:37 +0530

    Update TimeGenerator.java

--- src/main/java/com/talfinder/assessment/TimeGenerator.java
+++ src/main/java/com/talfinder/assessment/TimeGenerator.java
@@ -2,8 +2,19 @@ package com.talfinder.assessment;
 
 public class TimeGenerator {
 
-  public static String generateMaxTime(int[] digits) {
-    return "";
-  }
+    public static String generateMaxTime(int[] digits) {
+        /*
+         * NOTE:
+         * 1. Write your solution within this method
+         * 2. Do not make changes to the Class Name, Method Name or Method Signature
+         * 3. Use the Run-Java command to run main method
+         * 4. Use the BuildAndRunTests command to evaluate your solution
+         */
+        return "";
+    }
 
+    public static void main(String[] args) {
+        int[] digits = new int[]{0, 3, 2, 5};
+        System.out.println("Max Time : " + generateMaxTime(digits));
+    }
 }
